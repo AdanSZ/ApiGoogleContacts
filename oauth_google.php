9478f84182ae88c80af393a557b32d43883a282c
adan <adan.sandez21@gmail.com>
2016-02-26 16:02:25 -0700

    vendor

---
+++ oauth_google.php
@@ -0,0 +1,6 @@
+<?php
+
+	require_once("./wp-load.php");
+	echo get_template_directory_uri();
+	//require_once 'google-api-php-client/src/Google/autoload.php';// or wherever autoload.php is located
+
